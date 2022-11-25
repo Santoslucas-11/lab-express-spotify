1613ad584c45e9a5cb9b37f3ec101511c6e8e104
Lucas Santos <lucasantos112291@gmail.com>
2022-11-25 22:20:42 +0000

    partial done

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
+
 
 const app = express();
 
@@ -13,6 +15,57 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENTID,
+    clientSecret: process.env.SPOTIFY_CLIENTSECRET,
+    });
+
+    spotifyApi
+.clientCredentialsGrant()
+.then(data => spotifyApi.setAccessToken(data.body['access_token']))
+.catch(error => console.log('Something went wrong when retrieving an access token', error));
+
+
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/artist-search', (req, res) => {
+    spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+    console.log('The received data from the API: ', data.body.items);
+    res.render("artist-search", {result: data.body.items});
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+    });
+
+    app.get("/albums/:id", (req, res) => {
+        spotifyApi
+          .getArtistAlbums(req.params.id)
+          .then((data) => {
+            res.render("albums", { albums: data.body.items });
+            console.log(data.body);
+          })
+          .catch((error) => console.log("An error occured:", error));
+      });
+      
+      app.get("/albums/tracks/:id", (req, res) => {
+        spotifyApi
+          .getAlbumTracks(req.params.id, { limit: 5, offset: 1 })
+          .then((data) => {
+            res.render("tracks", { tracks: data.body.items });
+            console.log(data.body);
+          })
+          .catch((error) => console.log("An error occured:", error));
+      });
+      
+      app.listen(3000, () =>
+        console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+      );
